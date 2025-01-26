109656cc29365e9921509080741fe4360e7258b9
mahongqin <codepapercut@gmail.com>
2025-01-26 15:11:21 +0800

    Add serde

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Message {
+    message: String,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route(
+        "/",
+        get(|| async {
+            let message = Message {
+                message: "Hello, World!".to_string(),
+            };
+            Json(message)
+        }),
+    );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
